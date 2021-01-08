3375bc4826500f459fe146b91b3740e84c3e2cde
AleksCrow <Qwerton00@gmail.com>
2021-01-08 10:26:15 +0200

    Third commit - added Minfin, some fixes

--- src/main/java/com/rates/service/AverageRateService.java
+++ src/main/java/com/rates/service/AverageRateService.java
@@ -1,10 +1,9 @@
 package com.rates.service;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -23,13 +22,18 @@ public class AverageRateService {
 	
 	private MonobankService monobankService;
 	private PrivatbankService privatbankService;
+	private NationalbankService nationalbankService;
 	
 	private AverageRateRepository repository;
 
-	public AverageRateService(AverageRateRepository repository, MonobankService monobankService, PrivatbankService privatbankService) {
+	public AverageRateService(AverageRateRepository repository, 
+						MonobankService monobankService, 
+						PrivatbankService privatbankService,
+						NationalbankService nationalbankService) {
 		this.repository = repository;
         this.monobankService = monobankService;
         this.privatbankService = privatbankService;
+        this.nationalbankService = nationalbankService;
     }
 	
 	public List<AverageRate> getRates() {
@@ -60,19 +64,21 @@ public class AverageRateService {
 		
 		monobankService.findBetween(startTime, endTime).forEach(r -> averageRates.add(AverageRateUtils.converter(r)));
 		privatbankService.findBetween(startTime, endTime).forEach(r -> averageRates.add(AverageRateUtils.converter(r)));
+		nationalbankService.findBetween(startTime, endTime).forEach(r -> averageRates.add(AverageRateUtils.converter(r)));
 		
 		List<AverageRate> result = new ArrayList<>();
 		
-		Map<String, Double> mapBuy = getAverageBuy(averageRates);
-		Map<String, Double> mapSell = getAverageSell(averageRates);
+		Map<String, Integer> mapBuy = getAverageBuy(averageRates);
+		Map<String, Integer> mapSell = getAverageSell(averageRates);
 		
 		mapBuy.entrySet().forEach(a -> result.add(
 						new AverageRate(a.getKey(), 
 							"UAH", 
 							LocalDateTime.now(), 
-							(float) (a.getValue() / 10000), 
-							(float) (mapSell.get(a.getKey()) / 10000))));
+							((float) a.getValue() / 10000), 
+							((float) mapSell.get(a.getKey()) / 10000))));
 		
+		log.warn("{}", result.toString());
 		return result;
 	}
 	
@@ -81,32 +87,58 @@ public class AverageRateService {
 		
 		monobankService.findAllByCurrency().forEach(r -> averageRates.add(AverageRateUtils.converter(r)));
 		privatbankService.findAllByCurrency().forEach(r -> averageRates.add(AverageRateUtils.converter(r)));
+		nationalbankService.findAllByCurrency().forEach(r -> averageRates.add(AverageRateUtils.converter(r)));
 		
-		Map<String, Double> mapBuy = getAverageBuy(averageRates);
-		Map<String, Double> mapSell = getAverageSell(averageRates);
+		Map<String, Integer> mapBuy = getAverageBuy(averageRates);
+		Map<String, Integer> mapSell = getAverageSell(averageRates);
 		
 		mapBuy.entrySet().forEach(a -> repository.save(
 						new AverageRate(a.getKey(), 
 								"UAH", 
 								LocalDateTime.now(), 
-								(float) (a.getValue() / 10000), 
-								(float) (mapSell.get(a.getKey()) / 10000))));
+								((float) a.getValue()) / 10000, 
+								((float) mapSell.get(a.getKey()) / 10000))));
 	}
 	
 	private void loadData() {
 		monobankService.loadRatesData();
 		privatbankService.loadRatesData();
+		nationalbankService.loadRatesData();
+		
 	}
 	
-	private Map<String, Double> getAverageBuy(List<AverageRate> averageRates) {
-		return averageRates.stream()
+	private Map<String, Integer> getAverageBuy(List<AverageRate> averageRates) {
+		Map<String, Integer> map = averageRates.stream()
 				.collect(Collectors.groupingBy(AverageRate::getSourceCurrency, 
-						Collectors.averagingInt(r -> (int) (r.getRateBuy() * 10000))));
+						Collectors.summingInt(r -> (int) (r.getRateBuy() * 10000))));
+		
+		return calculateAverage(map, averageRates);
 	}
 	
-	private Map<String, Double> getAverageSell(List<AverageRate> averageRates) {
-		return averageRates.stream()
+	private Map<String, Integer> getAverageSell(List<AverageRate> averageRates) {
+		Map<String, Integer> map = averageRates.stream()
 				.collect(Collectors.groupingBy(AverageRate::getSourceCurrency, 
-						Collectors.averagingInt(r -> (int) (r.getRateSell() * 10000))));
+						Collectors.summingInt(r -> (int) (r.getRateSell() * 10000))));
+		
+		return calculateAverage(map, averageRates);
+	}
+	
+	private Map<String, Integer> calculateAverage(Map<String, Integer> map, List<AverageRate> averageRates) {
+		int usdCount = (int) averageRates.stream().filter(r -> r.getSourceCurrency().equals("USD")).count();
+		int rubCount = (int) averageRates.stream().filter(r -> r.getSourceCurrency().equals("RUB")).count();
+		int eurCount = (int) averageRates.stream().filter(r -> r.getSourceCurrency().equals("EUR")).count();
+		
+		Map<String, Integer> result = new HashMap<>();
+		if (usdCount != 0) {
+			result.put("USD", map.get("USD") / usdCount);
+		}
+		if (rubCount != 0) {
+			result.put("RUB", map.get("RUB") / rubCount);
+		}
+		if (eurCount != 0) {
+			result.put("EUR", map.get("EUR") / eurCount);
+		}
+		
+		return result;
 	}
 }
--- src/test/java/com/rates/CurrencyApplicationTests.java
+++ src/test/java/com/rates/CurrencyApplicationTests.java
@@ -1,13 +1,24 @@
 package com.rates;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.rates.controller.AverageRateController;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 class CurrencyApplicationTests {
 
+	@Autowired
+	private AverageRateController controller;
+	
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
